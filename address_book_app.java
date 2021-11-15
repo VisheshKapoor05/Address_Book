9d9c09331602da1a148ae1e800e373895625de87
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-15 15:47:57 +0530

    added loop to add multiple contacts in arraylist

--- address_book_app.java
+++ address_book_app.java
@@ -1,9 +1,11 @@
 package Address_Book;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class address_book_app {
-
+	
+	static ArrayList<Contacts> addressbook = new ArrayList<Contacts>();
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to address book");
@@ -15,18 +17,29 @@ public class address_book_app {
 		int decision = sc.nextInt();
 		
 		edit_addressBook contact = new edit_addressBook();
-		if(decision == 1)
-		{
-			contact.add_Contacts();
-		}
-		else if(decision == 2)
-		{
-			contact.edit_contact();
-		}
-		else if(decision == 3)
-		{
-			contact.delete_contact();
-		}
 		
+		int end_edits = 1 ;
+		while(end_edits != 0) {
+			if(decision == 1)
+			{
+				contact.add_Contacts();
+			}
+			else if(decision == 2)
+			{
+				contact.edit_contact();
+			}
+			else if(decision == 3)
+			{
+				contact.delete_contact();
+			}
+			
+			System.out.println("Press 1 to add a contact, 2 to edit, 3 to delete it, or 0 to finish the address book");
+			end_edits = sc.nextInt();
+			decision = end_edits;
+		}
+		System.out.println("contacts of Current Address book");
+		for(int i=0; i<addressbook.size(); i++) {
+			System.out.println(addressbook.get(i));
+		}
 	}
 }
