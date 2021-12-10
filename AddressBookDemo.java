69c50fe3d3c2036e170b52274b032c63a652bdb8
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-12-10 15:08:45 +0530

    UC8: Added searchContact method to search in a specific place across all addressbooks

--- AddressBookDemo.java
+++ AddressBookDemo.java
@@ -1,14 +1,17 @@
 package Address_Book;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import Address_Book.Service.AddressBookService;
 import Address_Book.entity.AddressBook;
+import Address_Book.entity.Contact;
 
 public class AddressBookDemo {
 	
-	static ArrayList<AddressBook> addressBooksList = new ArrayList<AddressBook>();
+	public static Set<AddressBook> addressBooksSet = new HashSet<AddressBook>();
 	
 	public static void main(String[] args) {
 		
@@ -20,30 +23,40 @@ public class AddressBookDemo {
 		System.out.println("Press 1 to add an address book");
 		int addressbookNum = sc.nextInt();
 		
-		int end_addressbooks = 1 ;
-		while(end_addressbooks != 0) {
+		AddressBookService addressBookServiceObj = new AddressBookService();
+		
+		while(addressbookNum != 0) {
+			
 			if(addressbookNum==1) 
 			{
 				System.out.println("Adding a new address book");
 				System.out.println("Enter the name of the address book: ");
 				String addressbookName = sc.next();
-				
-				AddressBookService addressBookServiceObj = new AddressBookService(); 
+				 
 				addressBookServiceObj.addAddressBook(addressbookName);
-				addressBooksList.add(addressBookServiceObj.newAddressBook);
+				addressBooksSet.add(addressBookServiceObj.newAddressBook);
+			}
+			else if(addressbookNum==2) {
+				System.out.println("Enter the city or state you want to the person to be searched in");
+				String place = sc.next();
+				
+				addressBookServiceObj.searchContact(place);
 			}
 			else
-				System.out.println("Please enter 1 to add an address book");
+				System.out.println("Please enter 1 or 2");
 			
-			System.out.println("list of Address book");
-			for(int i=0; i<addressBooksList.size(); i++) {
-				System.out.println(addressBooksList.get(i));
-			}
-			
+			System.out.println();
 			System.out.println("Press 1 again if you want to add another address book");
-			end_addressbooks = sc.nextInt();
-		
-		}
-		
+			System.out.println("or Press 2 to search any person across all the addressBooks");
+			System.out.println("or Press 0 to display the AddressBooks and end");
+			addressbookNum = sc.nextInt();
+			
+			if(addressbookNum == 0) {
+				System.out.println("list of Address book:");
+				for(AddressBook addressBookObj : addressBooksSet) {
+					System.out.println(addressBookObj);
+				}
+			}
+		}			
 	}
 }
--- Service/AddressBookService.java
+++ Service/AddressBookService.java
@@ -2,7 +2,9 @@ package Address_Book.Service;
 
 import java.util.Scanner;
 
+import Address_Book.AddressBookDemo;
 import Address_Book.entity.AddressBook;
+import Address_Book.entity.Contact;
 
 public class AddressBookService {
 	
@@ -24,16 +26,45 @@ public class AddressBookService {
 		int end_edits = 1;
 		while (end_edits != 0) {
 			if (decision == 1) {
-				contact.addContacts(newAddressBook.contactsList);
+				contact.addContacts(newAddressBook.contactsSet);
 			} else if (decision == 2) {
-				contact.editContact(newAddressBook.contactsList);
+				contact.editContact(newAddressBook.contactsSet);
 			} else if (decision == 3) {
-				contact.deleteContact(newAddressBook.contactsList);
+				contact.deleteContact(newAddressBook.contactsSet);
 			}
 
 			System.out.println("Press 1 to add a contact, 2 to edit, 3 to delete it, or 0 to finish the address book");
 			end_edits = sc.nextInt();
 			decision = end_edits;
 		}
+	}
+	
+	public void searchContact(String place) {
+		boolean placeFound = false;
+		boolean personFound = false;
+		
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the person first name you want to search");
+		String firstName = sc.next();
+		
+		for(AddressBook obj : AddressBookDemo.addressBooksSet) 
+		{
+			for(Contact person : obj.contactsSet) 
+			{
+				if(person.getCity().equals(place) || person.getState().equals(place)) 
+				{
+					placeFound = true;
+					if(person.getFirstName().equals(firstName)) {
+						System.out.println(person);
+						personFound = true;
+					}
+				}
+			}
+		}
+		if(placeFound == false)
+			System.out.println("No such city or state exists");
+		if(personFound == false)
+			System.out.println("No such person exists");
 	}
 }
