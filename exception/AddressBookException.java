feb1752f0665b79832da0955408098060285de46
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-12-12 12:52:35 +0530

    UC9: Added method to maintain 2 HashMap dictionaries of city-Persons & state-persons

---
+++ exception/AddressBookException.java
@@ -0,0 +1,12 @@
+package Address_Book.exception;
+
+public class AddressBookException extends Exception {
+	
+	public AddressBookException() {
+		super();
+	}
+	
+	public AddressBookException(String message) {
+		super(message);
+	}
+}
